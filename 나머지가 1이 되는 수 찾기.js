1d5eaee4f01a9bb287d19e14565e8773f9b0f886
jaegyun0320 <zkdbqhdb@naver.com>
2023-06-28 23:32:16 +0900

    프로그래머스 1주차풀이

---
+++ 나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    for(let x = 0; x <= n; x ++){
+        if( n % x === 1 ){
+        return x;
+                                }
+    
+    }
+    
+    
+}
---
+++ 서울에서 김서방 찾기.js
@@ -0,0 +1,6 @@
+function solution(seoul) {
+    seoul.indexOf("Kim")
+    const x = seoul.indexOf("Kim")
+    return ('김서방은 '+ x +'에 있다')
+    
+}
