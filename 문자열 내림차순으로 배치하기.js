53c11029c8bbede06dd8038c96b99e66bdf0b2c2
jaegyun0320 <zkdbqhdb@naver.com>
2023-06-29 21:42:18 +0900

    test

--- 문자열 내림차순으로 배치하기.js
+++
@@ -1,8 +0,0 @@
-function solution(s) {
-    let n = s.split("");
-    n.sort();
-    n.reverse();
-    let i = n.join('');
-    console.log(n.join(''))
-    return i;
-}
--- 서울에서 김서방 찾기.js
+++
@@ -1,6 +0,0 @@
-function solution(seoul) {
-    seoul.indexOf("Kim")
-    const x = seoul.indexOf("Kim")
-    return ('김서방은 '+ x +'에 있다')
-    
-}
